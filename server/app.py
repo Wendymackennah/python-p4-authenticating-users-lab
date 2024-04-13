66ad5515249ce960b8b979746548c7f92b016290
Wendymackennah <wendymackennah5@gmail.com>
2024-04-13 18:35:27 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, request, session
+from flask import Flask, jsonify, make_response, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -10,7 +9,6 @@ app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 
@@ -19,39 +17,64 @@ db.init_app(app)
 api = Api(app)
 
 class ClearSession(Resource):
-
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.pop('page_views', None)
+        session.pop('user_id', None)
         return {}, 204
 
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
 class ShowArticle(Resource):
-
     def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
-
+        session['page_views'] = session.get('page_views', 0) + 1
         if session['page_views'] <= 3:
+            article = Article.query.get(id)
+            if article:
+                return jsonify(article.to_dict()), 200
+            else:
+                return {'message': 'Article not found'}, 404
+        else:
+            return {'message': 'Maximum pageview limit reached'}, 401
 
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get('username')
+        if username:
+            user = User.query.filter(User.username == username).first()
+            if user:
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            else:
+                return {'message': 'User not found'}, 404
+        else:
+            return {'message': 'Username not provided'}, 400
 
-            return make_response(article_json, 200)
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
 
-        return {'message': 'Maximum pageview limit reached'}, 401
-
+class CheckSession(Resource):
+    def get(self):
+        if 'user_id' in session:
+            user_id = session['user_id']
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+            else:
+                return {'message': 'User not found'}, 404
+        else:
+            return {}, 401
+        
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
